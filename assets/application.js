a14dac219ba98cd5e6f3a6e8ef2dec5d283573b5
Abul Kashem <kashem.mcpd@gmail.com>
2022-06-14 21:41:39 +0600

    address page update 3

--- assets/application.js
+++ assets/application.js
@@ -7,15 +7,33 @@ if (document.getElementById("sortBy") != null) {
   });
 }
 
+// For Province Loaded
 
-// For Province Loaded 
+if (document.getElementById("AddressCountryNew") != null) {
+  document.getElementById("AddressCountryNew").addEventListener("change", function (e) {
+    province = this.options[this.selectedIndex].getAttribute("data-provinces");
+    provinceSelector = document.getElementById("AddressCountryNew");
+    provinceArray = JSON.parse(province);
 
-if(document.getElementById('AddressCountryNew') != null){
-  document.getElementById('AddressCountryNew').addEventListener('change', function(e){
-    province = this.options[this.selectedIndex].getAttribute('data-provinces')
-    provinceSelector = document.getElementById('AddressCountryNew')
-    provinceArray = JSON.parse(province)
+    // console.log(provinceArray);
 
-    console.log(provinceArray)
-  })
+    if (provinceArray.length < 1) {
+      provinceSelector.setAttribute('disabled', 'disabled')
+    } else {
+      provinceSelector.removeAttribute('disabled')
+    }
+
+    provinceSelector.innerHTML = ''
+    options = ''
+    for (let index = 0; index < provinceArray.length; index++) {
+      const element = array[index];
+
+      options += '<option value="' + provinceArray[index][0] + '">' + provinceArray[i][0] + '</option>'
+
+    }
+
+    provinceSelector.innerHTML = options
+
+
+  });
 }
