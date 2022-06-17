ea481e812be635a06fbbb5c7f7046d70decd1f78
Abul Kashem <kashem.mcpd@gmail.com>
2022-06-17 09:49:00 +0600

    important update

--- assets/application.js
+++ assets/application.js
@@ -76,12 +76,46 @@ if (localeItems.length > 0) {
 
 var productInfoAnchors = document.querySelectorAll("#productInfoAnchor");
 
-// console.log(productInfoAnchors);
+var productModal;
+
+if (document.getElementById("productInfoModal") != null) {
+  productModal = new bootstrap.Modal(
+    document.getElementById("productInfoModal"),
+    {}
+  );
+}
 
 if (productInfoAnchors.length > 0) {
   productInfoAnchors.forEach((item) => {
     item.addEventListener("click", (event) => {
-      console.log("Image anchor clicked");
+      var url = "/products/" + item.getAttribute("product-handle") + ".js";
+
+      fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+          console.log(data);
+
+          document.getElementById("productInfoImg").src = data.images[0];
+          document.getElementById("productInfoTitle").innerHTML = data.title;
+          document.getElementById("productInfoPrice").innerHTML = item.getAttribute("product-price");
+          document.getElementById("productInfoDescription").innerHTML = data.description;
+
+          var variants = data.variants;
+          var variantSelect = document.getElementById("modalItemID");
+
+          variantSelect.innerHTML = "";
+
+          variants.forEach(function (variant, index) {
+            console.log(variant);
+
+            variantSelect.options[variantSelect.options.length] = new Option(
+              variant.option1,
+              variant.id
+            );
+          });
+
+          productModal.show();
+        });
     });
   });
 }
