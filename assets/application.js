250c757632a05e38458a0726be89072f62d6dbdf
Abul Kashem <kashem.mcpd@gmail.com>
2022-06-17 10:06:36 +0600

    important update

--- assets/application.js
+++ assets/application.js
@@ -88,34 +88,7 @@ if (document.getElementById("productInfoModal") != null) {
 if (productInfoAnchors.length > 0) {
   productInfoAnchors.forEach((item) => {
     item.addEventListener("click", (event) => {
-      var url = "/products/" + item.getAttribute("product-handle") + ".js";
-
-      fetch(url)
-        .then((resp) => resp.json())
-        .then(function (data) {
-          console.log(data);
-
-          document.getElementById("productInfoImg").src = data.images[0];
-          document.getElementById("productInfoTitle").innerHTML = data.title;
-          document.getElementById("productInfoPrice").innerHTML = item.getAttribute("product-price");
-          document.getElementById("productInfoDescription").innerHTML = data.description;
-
-          var variants = data.variants;
-          var variantSelect = document.getElementById("modalItemID");
-
-          variantSelect.innerHTML = "";
-
-          variants.forEach(function (variant, index) {
-            console.log(variant);
-
-            variantSelect.options[variantSelect.options.length] = new Option(
-              variant.option1,
-              variant.id
-            );
-          });
-
-          productModal.show();
-        });
+      console.log("clicked");
     });
   });
 }
