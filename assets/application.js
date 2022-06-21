48222f88deec56dff8fb3c9c4abf4d6b7bf52a14
Abul Kashem <kashem.mcpd@gmail.com>
2022-06-21 22:55:55 +0600

    css form add to cart and cart pill information update

--- assets/application.js
+++ assets/application.js
@@ -85,49 +85,99 @@ if (document.getElementById("productInfoModal") != null) {
   );
 }
 
-
 var productInfoAnchors = document.querySelectorAll("#productInfoAnchor");
 
 var productModal;
 
-if( document.getElementById('productInfoModal') != null ) {
-    productModal = new bootstrap.Modal(document.getElementById('productInfoModal'), {});
+if (document.getElementById("productInfoModal") != null) {
+  productModal = new bootstrap.Modal(
+    document.getElementById("productInfoModal"),
+    {}
+  );
 }
 
-if(productInfoAnchors.length > 0) {
-    productInfoAnchors.forEach(item => {
-        item.addEventListener("click", event => {
-            
-            var url = '/products/' + item.getAttribute('product-handle') + '.js';
+if (productInfoAnchors.length > 0) {
+  productInfoAnchors.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      var url = "/products/" + item.getAttribute("product-handle") + ".js";
 
-            fetch(url)
-            .then((resp) => resp.json())
-            .then(function(data) {
-                console.log(data);
+      fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+          console.log(data);
 
-                document.getElementById("productInfoImg").src = data.images[0];
-                document.getElementById("productInfoTitle").innerHTML = data.title;
-                document.getElementById("productInfoPrice").innerHTML = item.getAttribute('product-price');
-                var length = 700;
-                var myString = data.description;
-                var myTruncatedString = myString.substring(0,length) + "...";
-                document.getElementById("productInfoDescription").innerHTML = myTruncatedString;//data.description;
+          document.getElementById("productInfoImg").src = data.images[0];
+          document.getElementById("productInfoTitle").innerHTML = data.title;
+          document.getElementById("productInfoPrice").innerHTML =
+            item.getAttribute("product-price");
+          var length = 700;
+          var myString = data.description;
+          var myTruncatedString = myString.substring(0, length) + "...";
+          document.getElementById("productInfoDescription").innerHTML =
+            myTruncatedString; //data.description;
 
-                var variants = data.variants;
-                var variantSelect = document.getElementById("modalItemID");
+          var variants = data.variants;
+          var variantSelect = document.getElementById("modalItemID");
 
-                variantSelect.innerHTML = '';
+          variantSelect.innerHTML = "";
 
-                variants.forEach(function( variant, index) {
-                    console.log(variant);
+          variants.forEach(function (variant, index) {
+            console.log(variant);
 
-                    variantSelect.options[variantSelect.options.length] = new Option(variant.option1, variant.id);
-                });
+            variantSelect.options[variantSelect.options.length] = new Option(
+              variant.option1,
+              variant.id
+            );
+          });
 
-                productModal.show();
-            });
-
-            
+          productModal.show();
         });
     });
+  });
+}
+
+modalAddToCartForm = document.querySelector("#addToCartForm");
+
+if (modalAddToCartForm != null) {
+  modalAddToCartForm.addEventListener("submit", function (e) {
+    let formData = {
+      items: [
+        {
+          id: document.getElementById("modalItemID").value,
+          quantity: document.getElementById("modalItemQuantity").value,
+        },
+      ],
+    };
+    fetch(window.Shopify.routes.root + "cart/add.js", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        update_cart();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  update_cart();
+});
+
+function update_cart() {
+  fetch("/cart.js")
+    .then((resp) => resp.json())
+    .then(
+      (data) =>
+        (document.getElementById("numberOfCartItems").innerHTML =
+          data.items.length)
+    )
+    .catch((err) => console.error(err));
 }
